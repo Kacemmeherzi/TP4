843baa73b432cff250b4b9c818d5fd2c34b67b68
kacem <kacemmeherzi52@gmail.com>
2024-05-16 14:31:20 +0100

    app componet added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,20 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from './navbar/navbar.component';
+import { TaskFormComponent } from './task-form/task-form.component';
+import { TasksListComponent } from './tasks-list/tasks-list.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,NavbarComponent,TaskFormComponent,TasksListComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'app';
+
+ 
 }
+
+
